e6456196187a5200ac5f8ccb9cb430bfe643e403
Earl Miles <merlin@logrus.com>
2011-12-02 11:21:10 -0800

    Issue #1261384 by MyXelf: Support D7 title_prefix and title_suffix on panes.

--- templates/panels-pane.tpl.php
+++ templates/panels-pane.tpl.php
@@ -25,9 +25,11 @@
     <?php print $admin_links; ?>
   <?php endif; ?>
 
+  <?php print render($title_prefix); ?>
   <?php if ($title): ?>
-    <h2 class="pane-title"><?php print $title; ?></h2>
+    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
   <?php endif; ?>
+  <?php print render($title_suffix); ?>
 
   <?php if ($feeds): ?>
     <div class="feed">
