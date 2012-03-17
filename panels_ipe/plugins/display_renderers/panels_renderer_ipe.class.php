fa6d4ba7235a3033c0640834a2b7bb6ba1e86d34
Sam Boyer <drupal@samboyer.org>
2012-03-17 12:54:41 -0400

    Issue #1396600: Fixed IPE Add / Delete / Move not working on regions with underscores in their name.

--- panels_ipe/plugins/display_renderers/panels_renderer_ipe.class.php
+++ panels_ipe/plugins/display_renderers/panels_renderer_ipe.class.php
@@ -169,8 +169,6 @@ class panels_renderer_ipe extends panels_renderer_editor {
     $output = theme('panels_ipe_region_wrapper', array('output' => $output, 'region_id' => $region_id, 'display' => $this->display, 'renderer' => $this));
     $classes = 'panels-ipe-region';
 
-    ctools_include('cleanstring');
-    $region_id = ctools_cleanstring($region_id);
     return "<div id='panels-ipe-regionid-$region_id' class='panels-ipe-region'>$output</div>";
   }
 
@@ -383,8 +381,6 @@ class panels_renderer_ipe extends panels_renderer_editor {
       $pane = $this->display->content[$pid];
     }
 
-    ctools_include('cleanstring');
-    $region_id = ctools_cleanstring($pane->panel);
     $this->commands[] = ajax_command_append("#panels-ipe-regionid-$region_id div.panels-ipe-sort-container", $this->render_pane($pane));
     $this->commands[] = ajax_command_changed("#panels-ipe-display-{$this->clean_key}");
   }
